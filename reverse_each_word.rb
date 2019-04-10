f1d7a47f0a40716b6de94a88f6fc010b07a2997f
Unsar Malik <unsar.malik24@gmail.com>
2019-04-10 04:21:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,7 +8,7 @@ def reverse_each_word_with_each(sentence)
 end
 
 def reverse_each_word(sentence)
-  array = string.split(" ") 
+  array = sentence.split(" ") 
   test_array = []
   array.collect do|sentence| 
     test_array << sentence.reverse 
